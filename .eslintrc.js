ffcccb085566dbf684eb5e3346aa624687a42381
Kevin Ashworth <kevinashworth@yahoo.com>
2025-03-04 06:51:59 -0800

    feat: Add ESLint rule for import/order

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,50 @@
 module.exports = {
   extends: ["expo", "prettier"],
   ignorePatterns: ["/dist/*"],
+  settings: {
+    "import/resolver": {
+      node: {
+        alias: {
+          map: [["@", "./src"]],
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+        },
+      },
+    },
+  },
+  rules: {
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true /* ignore case. Options: [true, false] */,
+        },
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "builtin",
+            position: "before",
+          },
+          {
+            pattern: "~/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "unknown",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+      },
+    ],
+  },
 };
