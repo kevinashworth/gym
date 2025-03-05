65a27c0ea329c2f7cd79c1f050aafbf83370db71
Kevin Ashworth <kevinashworth@yahoo.com>
2025-03-04 17:50:35 -0800

    feat: remove unused color dependency

--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,15 @@
-import Color from "color";
-
 export const spectrum = {
   primary: "#0047BA",
-  primaryLight: Color("#0047BA").lighten(0.4).rgb().toString(),
+  primaryLight: "rgb(5, 101, 255)",
   primaryContent: "#fff",
   secondary: "#71E2EA",
   success: "#00a13c",
   warning: "#fc7100",
   error: "#F13A1E",
-  errorLight: Color("#F13A1E").lighten(0.4).rgb().toString(),
+  errorLight: "rgb(247, 147, 132)",
   errorContent: "#fff",
   base1: "#fff",
   base1Content: "#303030",
-  base2Content: Color("#434343").alpha(0.72).rgb().toString(),
-  base3Content: Color("#434343").alpha(0.5).rgb().toString(),
+  base2Content: "rgba(67, 67, 67, 0.72)",
+  base3Content: "rgba(67, 67, 67, 0.5)",
 };
