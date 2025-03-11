17fac9e9acf37899a72989a28b845e0daebb3e13
Kevin Ashworth <kevinashworth@yahoo.com>
2025-03-11 10:23:40 -0700

    feat: Add page info to tab screens

--- src/app/(tabs)/action.tsx
+++ src/app/(tabs)/action.tsx
@@ -1,10 +1,14 @@
+import { Stack } from "expo-router";
 import { View, Text, StyleSheet } from "react-native";
+
+import { spectrum } from "@/theme";
 
 export default function ActionTab() {
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ headerShown: false }} />
       <Text>Action will go here</Text>
-      <Text>File is currently in src app (tabs) action.tsx</Text>
+      <Text style={styles.pageInfo}>src/app/(tabs)/action.tsx</Text>
     </View>
   );
 }
@@ -14,5 +18,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  pageInfo: {
+    borderTopColor: spectrum.base3Content,
+    borderTopWidth: 1,
+    color: spectrum.base1Content,
+    fontSize: 11,
+    fontWeight: 300,
+    marginVertical: 12,
+    paddingVertical: 12,
+    textAlign: "center",
   },
 });
--- src/app/(tabs)/wallet.tsx
+++ src/app/(tabs)/wallet.tsx
@@ -1,10 +1,22 @@
+import { Stack } from "expo-router";
 import { View, Text, StyleSheet } from "react-native";
+
+import { spectrum } from "@/theme";
 
 export default function WalletTab() {
   return (
     <View style={styles.container}>
-      <Text>Wallet</Text>
-      <Text>File is currently in src app (tabs) wallet.tsx</Text>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: spectrum.primary },
+          headerTintColor: spectrum.primaryContent,
+        }}
+      />
+      <Text>Wallet will go here</Text>
+      <Text style={styles.pageInfo}>
+        src/app/(tabs)/wallet.tsx{"\n"}
+        The title should be visible and have a "primary" background
+      </Text>
     </View>
   );
 }
@@ -14,5 +26,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  pageInfo: {
+    borderTopColor: spectrum.base3Content,
+    borderTopWidth: 1,
+    color: spectrum.base1Content,
+    fontSize: 11,
+    fontWeight: 300,
+    marginVertical: 12,
+    paddingVertical: 12,
+    textAlign: "center",
   },
 });
