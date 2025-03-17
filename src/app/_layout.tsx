7abe74ce0671d90a0eb572a019d0f53fac032665
Kevin Ashworth <kevinashworth@yahoo.com>
2025-03-16 19:10:23 -0700

    feat(layout): add network and app state management
    Adds network and app state management to the application layout. This
    ensures that the TanStack Query library is aware of the device's network
    connectivity and application focus status, allowing it to refetch data
    when the app regains focus or the network connection is restored.
    
    See https://tanstack.com/query/latest/docs/framework/react/react-native

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,50 @@
 import { useEffect, useState } from "react";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  focusManager,
+  onlineManager,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import * as Network from "expo-network";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { AppState, Platform } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import CustomHeader from "@/components/header";
+
+import type { AppStateStatus } from "react-native";
 
 // Keep the splash screen visible while we prepare
 SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient();
 
+// see https://tanstack.com/query/latest/docs/framework/react/react-native#online-status-management
+onlineManager.setEventListener((setOnline) => {
+  const eventSubscription = Network.addNetworkStateListener((state) => {
+    setOnline(!!state.isConnected);
+  });
+  return eventSubscription.remove;
+});
+
 export default function RootLayout() {
   const [appIsReady, setAppIsReady] = useState(false);
+
+  // see https://tanstack.com/query/latest/docs/framework/react/react-native#refetch-on-app-focus
+  function onAppStateChange(status: AppStateStatus) {
+    if (Platform.OS !== "web") {
+      focusManager.setFocused(status === "active");
+    }
+  }
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", onAppStateChange);
+
+    return () => subscription.remove();
+  }, []);
 
   useEffect(() => {
     async function prepare() {
