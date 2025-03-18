a46f0d8e9cf46c25328f018a739c5ae9bffed67e
Kevin Ashworth <kevinashworth@yahoo.com>
2025-03-17 22:48:12 -0700

    GitButler is pissing me off

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import {
-  // Pressable,
   StyleSheet,
   Text,
+  TextStyle,
   TouchableOpacity,
   View,
+  ViewStyle,
 } from "react-native";
 
 import { spectrum } from "@/theme";
@@ -13,14 +14,100 @@ import { spectrum } from "@/theme";
 import Icon from "./icon";
 
 import type { IconName } from "./icon";
-interface Props {
+
+type Sizes = "sm" | "md" | "lg" | "xl";
+type Variants =
+  | "primary"
+  | "secondary"
+  | "outline"
+  | "black"
+  | "white"
+  | "default";
+
+type ButtonSizesContainer = Record<Sizes, ViewStyle>;
+type ButtonSizesLabel = Record<Sizes, TextStyle>;
+type ButtonVariantsContainer = Record<Variants, ViewStyle>;
+type ButtonVariantsLabel = Record<Variants, TextStyle>;
+
+const buttonSizesContainer: ButtonSizesContainer = {
+  sm: { borderRadius: 12, padding: 4, paddingHorizontal: 8 },
+  md: { borderRadius: 12, padding: 6, paddingHorizontal: 12 },
+  lg: { borderRadius: 16, padding: 8, paddingHorizontal: 16 },
+  xl: { borderRadius: 20, padding: 10, paddingHorizontal: 20 },
+};
+
+const buttonVariantsContainer: ButtonVariantsContainer = {
+  primary: {
+    backgroundColor: spectrum.primary,
+    borderColor: spectrum.primary,
+    borderWidth: 1,
+  },
+  secondary: {
+    backgroundColor: spectrum.secondary,
+    borderColor: spectrum.secondary,
+    borderWidth: 1,
+  },
+  outline: {
+    backgroundColor: "transparent",
+    borderColor: spectrum.gray1,
+    borderWidth: 1,
+  },
+  black: {
+    backgroundColor: spectrum.black,
+    borderColor: spectrum.black,
+    borderWidth: 1,
+  },
+  white: {
+    backgroundColor: spectrum.white,
+    borderColor: spectrum.white,
+    borderWidth: 1,
+  },
+  default: {
+    backgroundColor: spectrum.base1,
+    borderColor: spectrum.base1,
+    borderWidth: 1,
+  },
+};
+
+const buttonSizesLabel: ButtonSizesLabel = {
+  sm: { fontSize: 12, fontWeight: 400 },
+  md: { fontSize: 14, fontWeight: 500 },
+  lg: { fontSize: 16, fontWeight: 500 },
+  xl: { fontSize: 18, fontWeight: 500 },
+};
+
+const buttonVariantsLabel: ButtonVariantsLabel = {
+  primary: { color: spectrum.white },
+  secondary: { color: spectrum.gray1 },
+  outline: { color: spectrum.gray1 },
+  black: { color: spectrum.white },
+  white: { color: spectrum.black },
+  default: { color: spectrum.primary },
+};
+
+interface ButtonProps {
+  buttonStyle?: ViewStyle;
+  disabled?: boolean;
   icon?: React.ReactElement;
   iconName?: IconName;
   label?: string;
   onPress: () => void;
+  size?: Sizes;
+  variant?: Variants;
+  withoutShadow?: boolean;
 }
 
-export default function Button({ icon, iconName, label, onPress }: Props) {
+export default function Button({
+  buttonStyle,
+  disabled = false,
+  icon,
+  iconName,
+  label,
+  onPress,
+  size = "md",
+  variant = "primary",
+  withoutShadow = false,
+}: ButtonProps) {
   const IconComponent = icon ? (
     React.cloneElement(icon, { color: spectrum.gray1 })
   ) : iconName ? (
