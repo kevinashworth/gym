0f5664c9bfb1e1f76a482bef1095786740af30b7
Kevin Ashworth <kevinashworth@yahoo.com>
2025-03-20 21:19:01 -0700

    Red squigglies

--- babel.config.js
+++ babel.config.js
@@ -4,3 +4,21 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
   };
 };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          alias: {
+            "@": "./src",
+          },
+        },
+      ],
+    ],
+  };
+};
