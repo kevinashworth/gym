682c5a20ba9a38455a6eeb7e775b83c6174a490c
Kevin Ashworth <kevinashworth@yahoo.com>
2025-03-21 22:08:16 -0700

    New user store
    stop using zustand devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
 export { default as useAuthStore } from "./auth";
 export { default as useDevStore } from "./dev";
+export { default as useUserStore } from "./user";
 export { default as useTestStore } from "./test";
