f6f28d821fae0de0208221e4aee3a44d76a9e8c1
Kevin Ashworth <kevinashworth@yahoo.com>
2025-03-22 08:47:08 -0700

    Fix babel config
    A duplicate had worked its way in. I blame autocomplete.

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-  };
-};
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
     plugins: [
       [
         "module-resolver",
